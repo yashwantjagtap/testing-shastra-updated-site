fd94c9cf6d807ec9c8d459e6adb8ed9a178d8bbc
Utirna Examination Unit <utirna.akola@gmail.com>
2021-09-15 12:42:06 +0530

    Project Updated

--- public/js/handlers/webinar.handler.js
+++ public/js/handlers/webinar.handler.js
@@ -1,23 +1,28 @@
-$(function(){
-    $('#webinarRegistration').on('click',function(event){
-        event.preventDefault();
-        var form = $('#webinarForm')[0];
-        var formData = new FormData(form);
-        var sendData = {}
-        formData.forEach(function(value,name){
-            sendData[name] = value;
-        })
-        $.ajax({
-            url:"/save-webinar-candidate",
-            type:"post",
-            data:sendData
-        }).done(function(data){
-            alert('Saved');
-            console.log(data);
-        }).fail(function(error){
-            alert('error');
-            console.log(error);
-        })
+$(function () {
+  $("#webinarRegistration").on("click", function (event) {
+    event.preventDefault();
+    var form = $("#webinarForm")[0];
+    var formData = new FormData(form);
+    var sendData = {};
+    formData.forEach(function (value, name) {
+      sendData[name] = value;
     });
-    
+    $.ajax({
+      url: "/save-webinar-candidate",
+      type: "post",
+      data: sendData,
+    })
+      .done(function (data) {
+        alert("Saved");
+        console.log(data);
+      })
+      .fail(function (error) {
+        alert("error");
+        console.log(error);
+      });
+  });
+
+  $(document).on("click", "#registerModal", function () {
+    alert("submit clicked");
+  });
 });
