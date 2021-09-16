1264be8209343dc87ff75c58cf6cd5af5fc4f423
yashwantjagtap <yashwantjagtap1540@gmail.com>
2021-09-16 11:38:28 +0530

    final changes

--- app/config/dist/db.connect.dev.js
+++ app/config/dist/db.connect.dev.js
@@ -6,11 +6,11 @@ var mysql = require("mysql2"); // node-mysql2 module
 var myConnection = require("express-myconnection"),
     // express-myconnection module
 dbOptions = {
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
+  host: "localhost",
+  user: "root",
+  password: "1234",
   port: 3306,
-  database: 'testing_shastra'
+  database: "testing_shastra"
 };
 
 exports.myConnection = myConnection;
